3d28c350ce59280a45afcf91f0929667fd2aec34
cadebullen <111130091+cadebullen@users.noreply.github.com>
2022-10-11 10:35:56 -0400

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Palindrome{   
+      public static boolean isPalindrome(String s){
+      if (s.length() < 2){ //a string that has 0 or 1 letter is a palindrome
+         return true;
+      }
+      else if (s.charAt(0) == s.charAt(s.length()-1)){ // checks if the first letter of the string equals the last latter of the string
+         return (isPalindrome(s.substring(1,s.length()-1))); //after the first check, it checks a substring not containning the first two letters
+      }                                                      // by calling itself (recursive)
+      else{
+         return false;
+      }
+   }
+   public static void main (String[] args){
+      String s;
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter a string that is a Palindrome: ");
+      s = scan.next();
+     
+      if (isPalindrome(s))
+         System.out.println("This is a palindrome: " + s); // if the recursive method is true
+      else
+         System.out.println("This is not a palindrome: " + s); // if the recursive method is false
+   }
+}
+   
+
+   
